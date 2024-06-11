c642b2b4d15470838b2edba3e780cfc2566460c6
Esteban <brandonlopezmonroy@gmail.com>
2024-06-11 07:23:39 -0500

    cambios

--- user/src/redux/actions.js
+++
@@ -1,8 +0,0 @@
-import { GET_GAMES } from './action-types'
-
-export const getGames = () =>{
-    return async (dispatch) =>{
-        const response = await axios.get('http://localhost:3001/videogame/')
-        return dispatch({ type: GET_GAMES, payload: response.data })
-    }
-}
